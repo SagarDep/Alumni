bc1c621ac95ab3d96ba7e1b9b8d369c47f1a803f
Ashish Surana <ashishsurana@ymail.com>
2016-03-05 18:11:36 +0530

    Circular Images done

---
+++ app/src/main/java/com/example/ashish/alumini/ListVar.java
@@ -0,0 +1,57 @@
+package com.example.ashish.alumini;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.media.Image;
+import android.util.Log;
+
+/**
+ * Created by ashish on 3/3/16.
+ */
+public class ListVar {
+    Image image ;
+    String name ;
+    String year_passing;
+    String designation;
+    String location;
+    String fb_link;
+    String in_link;
+    String tw_link;
+    Bitmap bitmap;
+    public ListVar(String name2, Bitmap bitmap2){
+        name = name2;
+       this.bitmap= Bitmap.createBitmap(bitmap2);
+        if (bitmap==null)
+            Log.d("WTF","WTF");
+        if (bitmap!=null)
+            Log.d("WTF","BITMAP Not null" );
+    }
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+//        bitmap.recycle();
+
+        return output;
+    }
+}
---
+++ app/src/main/java/com/example/ashish/alumini/MemberAdapter.java
@@ -0,0 +1,78 @@
+package com.example.ashish.alumini;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ashish on 3/3/16.
+ */
+public class MemberAdapter extends ArrayAdapter<ListVar>{
+    List<ListVar> List_members;
+    public MemberAdapter(Context context,int rid, List<ListVar> list){
+        super(context,rid,list);
+        List_members=list;
+    }
+
+    @Override
+    public ListVar getItem(int position) {
+        return List_members.get(position);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ListVar  temp = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            convertView = vi.inflate(R.layout.simple_list_item, null);
+        }
+
+        Holder holder = new Holder();
+        holder.tv_name = (TextView) convertView.findViewById(R.id.textView_name);
+        holder.tv_designation = (TextView) convertView.findViewById(R.id.textView_designation);
+        holder.tv_location = (TextView) convertView.findViewById(R.id.textView_location);
+        holder.tv_passing_year = (TextView) convertView.findViewById(R.id.textView_passing_year);
+        holder.imageView_profilePic= (ImageView) convertView.findViewById(R.id.profileimage);
+
+
+        // -----------------------PROFILE PIC-------------------
+
+        holder.imageView_profilePic.setImageBitmap(ListVar.getCircleBitmap(temp.bitmap));
+
+
+        holder.tv_name.setText(temp.name);
+        holder.tv_designation.setText("Sr. Engineer" + " at " + "Dummy Company");
+        holder.tv_passing_year.setText("2000 CS");
+        holder.tv_location.setText("Somewhere");
+
+        return convertView;
+    }
+    public static class Holder{
+        public TextView tv_name ;
+        public TextView tv_designation ;
+        public TextView tv_location ;
+        public TextView tv_passing_year ;
+        public ImageView imageView_profilePic;
+
+    }
+
+}
