884cf96294692e5da42e6e03f0df84b23a05bd55
Ashish Surana <ashishsurana@ymail.com>
2016-03-10 15:55:53 +0530

    Edit Profile and view Prifile working perfectly

---
+++ app/src/main/java/com/example/ashish/alumini/onSettingPressed.java
@@ -0,0 +1,7 @@
+package com.example.ashish.alumini;
+
+/**
+ * Created by ashish on 9/3/16.
+ */
+public class onSettingPressed {
+}
--- app/src/main/java/com/example/ashish/alumini/profile.java
+++ app/src/main/java/com/example/ashish/alumini/profile.java
@@ -3,26 +3,17 @@ package com.example.ashish.alumini;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 /**
  * Created by ashish on 4/3/16.
  */
-public class onClickMember extends Activity{
+public class profile extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.afterlistclick);
-
         Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.image);
         ImageView mImage = (ImageView) findViewById(R.id.imageView_profilepic);
         mImage.setImageBitmap(ListVar.getCircleBitmap(bm));
